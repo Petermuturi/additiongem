6ae3497ebdd7584c8ca238c3e5954aac75704cfa
Peter Muturi <pmuturi65@gmail.com>
2015-08-18 08:43:17 +0300

    a simple addtion gem

---
+++ lib/add.rb
@@ -0,0 +1,13 @@
+require "add/version"
+
+module Add
+  def add
+puts "Hey there.we'll be adding two numbers today"
+puts "enter first number"
+num = gets.chomp
+puts "enter second number"
+num2 = gets.chomp
+fin = num + num2
+puts "the addition is: "+ fin
+end
+end
